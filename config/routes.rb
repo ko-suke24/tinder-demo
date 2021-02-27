1a1c17587877729ead58f5ecc80b97a57bc958c5
ko-suke24 <kosuke0322@gmail.com>
2021-02-27 18:56:17 +0900

    profile edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             controllers: { registrations: 'registrations' }
   root 'top#index'
 
   resources :users, only: [:show]
